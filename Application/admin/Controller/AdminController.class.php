8de873c6679f93f97e5e3a21bc0af24e462ddbff
roy <rxlisbest@gmail.com>
2014-02-19 22:55:30 +0800

    文章发布网站，初始化，静态页加载

---
+++ Application/admin/Controller/AdminController.class.php
@@ -0,0 +1,13 @@
+<?php
+// 本类由系统自动生成，仅供测试用途
+namespace admin\Controller;
+use Think\Controller;
+class AdminController extends Controller {
+    public function index(){
+    	$this->display("index");
+    }
+
+    public function helloworld(){
+    	$this->display("login");
+    }
+}
